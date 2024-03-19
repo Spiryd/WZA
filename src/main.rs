0e98b7a7024f876a7469c51993e3592614f4f261
Spiryd <neumann.maks@gmail.com>
2024-03-19 23:16:25 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod gaussian_number;
+mod polynomials;
 
 fn main() {
     let a = gaussian_number::GaussanNumber::new(3, 4);
@@ -7,4 +8,8 @@ fn main() {
     println!("GCD: {}", gcd);
     let lcm = a.lcm(&b);
     println!("LCM: {}", lcm);
+    let p1 = polynomials::Polynomial::new(vec![1., 0., 1.0]);
+    let p2 = polynomials::Polynomial::new(vec![1.0, 2., 1.]);
+    println!("GCD: {:?}", p1.gcd(p2.clone()));
+    println!("LCM: {:?}", p1.lcm(p2));
 }
